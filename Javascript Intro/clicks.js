caa24711f09bb09724c822fa5d3c89eb419622e2
MagicalLilicorn <lily4ever@msn.com>
2014-02-20 18:00:28 -0800

    Starting on JS

---
+++ Javascript Intro/clicks.js
@@ -0,0 +1,7 @@
+/*global $*/
+/*jslint sloppy:true, browser: true*/
+$("h1.page-title").text("It worked!");
+$(window).on("click", function (e) {
+    $('img.logo').css({left:e.clientX,
+                       top: e.clientY});
+});
